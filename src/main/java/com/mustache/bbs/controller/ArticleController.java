f51fe6355fb213089f4e5781fdf386590281e926
JaeHyun <wogus0518@naver.com>
2022-11-07 14:16:01 +0900

    RequestMapping 적용

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -5,17 +5,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@Slf4j // 로깅을 위한 어노테이션 log를 사용할 수있다.
+@Slf4j
+@RequestMapping("/articles")
 public class ArticleController {
 
-    @GetMapping(value = "/articles/new")
+    @GetMapping(value = "/new")
     public String newArticleForm() {
         return "articles/new";
     }
 
-    @PostMapping(value = "/articles/posts")
+    @PostMapping(value = "/posts")
     public String createArticle(ArticleDto form) {
         // 실무에서 println 안씀 로그를 쓴다(서버에서 일어나는 일을 기록하는것)
         log.info(form.toString());
