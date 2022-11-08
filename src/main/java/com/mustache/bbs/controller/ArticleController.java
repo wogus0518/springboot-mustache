8b155adc46b016ef90e4a1cd82eaedbe395039ad
JaeHyun <wogus0518@naver.com>
2022-11-08 15:40:52 +0900

    show, list 추가

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -6,9 +6,14 @@ import com.mustache.bbs.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @Slf4j
@@ -17,6 +22,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ArticleController {
 
     private final ArticleRepository articleRepository;
+
+    @GetMapping
+    public String redirectList() {
+        return "redirect:/articles/list";
+    }
+
+    @GetMapping("/list")
+    public String selectAll(Model model) {
+        List<Article> articles = articleRepository.findAll();
+        model.addAttribute("articles", articles);
+        return "list";
+    }
 
     @GetMapping(value = "/new")
     public String newArticleForm() {
@@ -30,6 +47,17 @@ public class ArticleController {
         Article article = form.toEntity();
         Article save = articleRepository.save(article);
         log.info("id={}", save.getId());
-        return "";
+        return "redirect:/articles/" + save.getId();
+    }
+
+    @GetMapping("/list/{id}")
+    public String selectSingle(@PathVariable Long id, Model model) {
+        Optional<Article> optArticle = articleRepository.findById(id);
+        if (optArticle.isPresent()) {
+            model.addAttribute("article", optArticle.get());
+            return "show";
+        } else {
+            return "error";
+        }
     }
 }
